61a64929d5eefb1ba4c2f77c10009b7e5407e7b9
Esteban Camargo <estebmaister@gmail.com>
2020-08-02 19:35:31 -0400

    🚧 Practice Code

---
+++ app/screens/practice/ViewImageScreen.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+import colors from "../../config/colors";
+
+export default function ViewImageScreen() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.closeIcon}></View>
+      <View style={styles.deleteIcon}></View>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/app.png")}
+      ></Image>
+      <Text></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.primary,
+    position: "absolute",
+    top: 40,
+    left: 30,
+  },
+  container: {
+    backgroundColor: colors.dark,
+    flex: 1,
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.secondary,
+    position: "absolute",
+    top: 40,
+    right: 30,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
